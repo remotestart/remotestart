26223b754e2e982f2736c155e25f9fa3227f27e9
JahzielB <jahzielb@gmail.com>
2020-08-13 14:31:30 -0500

    added gitignore and did initial setup

---
+++ src/main/java/com/capstone/remotestart/RemotestartApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.remotestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotestartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemotestartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/remotestart/RemotestartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.remotestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotestartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
