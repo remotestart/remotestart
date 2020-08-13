e8da79dcaa7bfd78f756d2df0ba3703356744701
JahzielB <jahzielb@gmail.com>
2020-08-13 16:58:07 -0500

    created repositories for role and user models

---
+++ src/main/java/com/capstone/remotestart/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.capstone.remotestart.repositories;
+
+import com.capstone.remotestart.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
