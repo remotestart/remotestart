93db72ba80f922a2e23cf6e0f823dab0e73bb9fd
JahzielB <jahzielb@gmail.com>
2020-08-14 11:45:56 -0500

    set up basic spring security and a sign-up page

--- src/main/java/com/capstone/remotestart/models/User.java
+++ src/main/java/com/capstone/remotestart/models/User.java
@@ -58,6 +58,13 @@ public class User {
         this.password = password;
     }
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
 //Getters and Setters
     public long getId() {
         return id;
