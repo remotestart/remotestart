c6cd5cb1dade793fc845f4a8d1d87360b93a8dbc
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-17 11:24:56 -0500

    subtasks can now be created

--- src/main/java/com/capstone/remotestart/controllers/SubtaskController.java
+++ src/main/java/com/capstone/remotestart/controllers/SubtaskController.java
@@ -1,16 +1,31 @@
 package com.capstone.remotestart.controllers;
 
 import com.capstone.remotestart.models.Subtask;
+import com.capstone.remotestart.repositories.SubtaskRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SubtaskController {
 
+    private SubtaskRepository subtaskDao;
+
+    public SubtaskController(SubtaskRepository subtaskDao) {
+        this.subtaskDao = subtaskDao;
+    }
+
     @GetMapping("/subtask/create")
-    public String createSubtask(Model model){
+    public String createSubtaskForm(Model model){
         model.addAttribute("subtask", new Subtask());
-        return "";
+        return "subtasks/create-subtask";
+    }
+
+    @PostMapping
+    public String saveSubtask(@ModelAttribute Subtask subtask){
+        subtaskDao.save(subtask);
+        return "redirect:/task";
     }
 }
