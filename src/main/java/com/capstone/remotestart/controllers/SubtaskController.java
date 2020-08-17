de4848b8c33388a41a4a887c2d0be1c31291a6e2
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-17 10:50:54 -0500

    adding links to navbar

---
+++ src/main/java/com/capstone/remotestart/controllers/SubtaskController.java
@@ -0,0 +1,16 @@
+package com.capstone.remotestart.controllers;
+
+import com.capstone.remotestart.models.Subtask;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SubtaskController {
+
+//    @GetMapping("/subtask/create")
+//    public String createSubtask(Model model){
+//        model.addAttribute("subtask", new Subtask());
+//        return ""
+//    }
+}
