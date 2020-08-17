61e79c8b1e109fd9cd28f14eca30394d4d0e0ec6
Dylan Leifeste <ldylan01@gmail.com>
2020-08-17 16:17:10 -0500

    got users to show on dropdown

--- src/main/java/com/capstone/remotestart/controllers/TaskController.java
+++ src/main/java/com/capstone/remotestart/controllers/TaskController.java
@@ -1,7 +1,12 @@
 package com.capstone.remotestart.controllers;
 
 import com.capstone.remotestart.models.Task;
+import com.capstone.remotestart.models.Team;
+import com.capstone.remotestart.models.User;
+import com.capstone.remotestart.models.UserTeamRoleLink;
 import com.capstone.remotestart.repositories.TaskRepository;
+import com.capstone.remotestart.repositories.TeamRepository;
+import com.capstone.remotestart.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +18,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TaskController {
 
     private TaskRepository taskDao;
+    private UserRepository userDao;
+    private TeamRepository teamDao;
 
-    public TaskController(TaskRepository taskDao) {
+    public TaskController(TaskRepository taskDao, UserRepository userDao, TeamRepository teamDao) {
         this.taskDao = taskDao;
+        this.userDao = userDao;
+        this.teamDao = teamDao;
     }
 
     @GetMapping("/task/create")
     public String createTask(Model model) {
         model.addAttribute("task", new Task());
+        model.addAttribute("users", userDao.findAll());
         return "tasks/create-task";
     }
 
@@ -35,4 +45,5 @@ public class TaskController {
         model.addAttribute("task", taskDao.findAll());
         return "tasks/task";
     }
+
 }
