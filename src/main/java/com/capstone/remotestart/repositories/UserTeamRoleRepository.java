082470fe507dfe487e7c93ae05b4bb48bf853d8c
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-17 16:44:59 -0500

    commented out code

--- src/main/java/com/capstone/remotestart/repositories/UserTeamRoleRepository.java
+++ src/main/java/com/capstone/remotestart/repositories/UserTeamRoleRepository.java
@@ -1,7 +1,11 @@
 package com.capstone.remotestart.repositories;
 
+import com.capstone.remotestart.models.User;
 import com.capstone.remotestart.models.UserTeamRoleLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserTeamRoleRepository extends JpaRepository <UserTeamRoleLink, Long> {
+//    List<User> findAllByTeamId
 }
