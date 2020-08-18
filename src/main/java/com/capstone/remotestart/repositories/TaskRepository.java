c8fc630c033a5bc62e3a25129fc1b4b9046357b8
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-18 13:55:29 -0500

    logged in user can view assigned tasks from project page

--- src/main/java/com/capstone/remotestart/repositories/TaskRepository.java
+++ src/main/java/com/capstone/remotestart/repositories/TaskRepository.java
@@ -2,9 +2,12 @@ package com.capstone.remotestart.repositories;
 
 import com.capstone.remotestart.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findAllByUserId(Long id);
+    @Query(value = "SELECT * FROM tasks WHERE project_id = :project_id AND user_id = :user_id", nativeQuery = true)
+    List<Task> findAllByUserAndProjectId(@Param("project_id") long projectId, @Param("user_id") long userId);
 }
