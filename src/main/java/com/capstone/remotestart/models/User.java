c15ea14487208d3c387818e7344f77d748e53ca1
JahzielB <jahzielb@gmail.com>
2020-08-18 10:00:40 -0500

    fixed users table

--- src/main/java/com/capstone/remotestart/models/User.java
+++ src/main/java/com/capstone/remotestart/models/User.java
@@ -29,7 +29,6 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(columnDefinition = "default false")
     private boolean isEnabled;
 
     //relation to task
