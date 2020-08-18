39fd6cbc573a2d68bf755c412480551bfdd02aa0
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-18 11:35:30 -0500

    users assigned to team now show up on team page

--- src/main/java/com/capstone/remotestart/repositories/UserRepository.java
+++ src/main/java/com/capstone/remotestart/repositories/UserRepository.java
@@ -2,8 +2,16 @@ package com.capstone.remotestart.repositories;
 
 import com.capstone.remotestart.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findByEmailIgnoreCase(String email);
+
+    @Query(value = "SELECT user_id FROM user_team_role WHERE team_id = :id", nativeQuery = true)
+    List<Long> allUsersByTeamId(@Param("id") long id);
+
 }
