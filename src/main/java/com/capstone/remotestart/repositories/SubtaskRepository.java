42989c895e914402ef5a88461deaae59ae76dd6b
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-19 10:27:20 -0500

    logged in users can view assigned tasks and all subtasks

--- src/main/java/com/capstone/remotestart/repositories/SubtaskRepository.java
+++ src/main/java/com/capstone/remotestart/repositories/SubtaskRepository.java
@@ -3,5 +3,8 @@ package com.capstone.remotestart.repositories;
 import com.capstone.remotestart.models.Subtask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubtaskRepository extends JpaRepository<Subtask, Long> {
+    List<Subtask> findAllByTaskId(Long id);
 }
