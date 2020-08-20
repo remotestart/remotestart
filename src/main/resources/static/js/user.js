dc7527164455349cc136eb7c3da6155fa761a27d
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-20 12:01:42 -0500

    Search functionality for adding users to teams updated with JS

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let bucket = document.querySelector("#bucket");
+let userSearch = document.querySelector("#user-search");
+
+let showUsers = (users) => {
+    for (let i = 0; i < users.length; i++){
+        bucket.innerHTML +=
+            "<div class='w-100 d-flex justify-content-center'>" +
+            "<div class='card w-50 my-3' style='width: 18rem'>" +
+            "<div class='card-header text-center font-weight-bold'>" + users[i].firstName + "</div>" +
+            "<div class='card-body d-flex flex-column'>" +
+            "<a href=" + "/team/" + users[i].id + " " + "class='btn btn-outline-info align-self-end'>Add User</a>" +
+            "</div>" +
+            "</div>" +
+            "</div>";
+    }
+}
+showUsers(users);
+
+let updateUsers = () => {
+    let filteredUsers = []
+    users.forEach(function (user) {
+        if (user.firstName.includes(userSearch.value)){
+            filteredUsers.push(user)
+        }
+    })
+    bucket.innerHTML =  "";
+    showUsers(filteredUsers);
+}
