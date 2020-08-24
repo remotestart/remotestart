97c5fac67e760d16d97639f704d41b77d6e7aa70
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-24 11:50:51 -0500

    Team Lead can complete a project

--- src/main/java/com/capstone/remotestart/repositories/ProjectRepository.java
+++ src/main/java/com/capstone/remotestart/repositories/ProjectRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.sql.Date;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
@@ -19,4 +20,9 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Modifying
     @Query(value = "UPDATE projects SET name = :name, description = :description, start_date = :startDate, deadline = :deadline WHERE id = :id", nativeQuery = true)
     void editProjectById(@Param("id") long id ,@Param("name") String name, @Param("description") String description, @Param("startDate") String startDate, @Param("deadline") String deadline);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE projects SET completion_date = :completion_date WHERE id = :id", nativeQuery = true)
+    void updateProjectCompletionDateById(@Param("id") long id, @Param("completion_date") Date completionDate);
 }
