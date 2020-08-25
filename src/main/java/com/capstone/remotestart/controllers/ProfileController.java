7b4a22e1ce80d46adf537e5a7859beb90fcba483
Dylan Leifeste <ldylan01@gmail.com>
2020-08-25 16:24:38 -0500

    passwords now get hashed when updating

--- src/main/java/com/capstone/remotestart/controllers/ProfileController.java
+++ src/main/java/com/capstone/remotestart/controllers/ProfileController.java
@@ -97,7 +97,6 @@ public class ProfileController {
         public String saveNewPassword(@PathVariable Long id, @ModelAttribute User user){
 
         String hash = passwordEncoder.encode(user.getPassword());
-//        userDao.getOne(id).setPassword(hash);
         userDao.editPassword(id, hash);
 
         return "redirect:/profile";
