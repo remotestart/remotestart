2a5f5f9361f28135f465e656d4e88756d3af5d2f
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-29 16:59:11 -0500

    styling for all tasks page

--- Database/seeder.sql
+++ Database/seeder.sql
@@ -20,15 +20,12 @@ values (),
        ();
 
 # Projects
-insert into projects ( name, description, start_date, deadline, completion_date)
-values ( 'Hana and Alice (Hana to Arisu)', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', '2019-01-02', '2019-11-13', '2020-03-11');
+
 
 # Tasks
-insert into tasks ( title, description)
-values ( 'Zakochani', 'Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.');
+
 
 # Subtasks
-insert into sub_tasks ( title, description)
-values ( 'Christopher Strong', 'Fusce consequat. Nulla nisl. Nunc nisl.');
+
 
 
