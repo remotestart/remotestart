7046a61049f52f09d65853283cb97e8c2bbc422e
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-31 12:28:34 -0500

    changed font and spacing

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -6,15 +6,14 @@ let userSearch = document.querySelector("#user-search");
 let showUsers = (users) => {
     for (let i = 0; i < users.length; i++){
         bucket.innerHTML +=
-            "<div class='w-100 d-flex justify-content-between my-2'>" +
+            "<div class='w-100 d-flex justify-content-between my-2 pb-3'>" +
             // "<div class='card w-50 my-3' style='width: 18rem'>" +
-            "<div class='text-center font-weight-bold'>" + users[i].firstName + " " + users[i].lastName + "</div>" +
+            "<div class='p-0 m-0 d-flex align-items-center h5'>" + users[i].firstName + " " + users[i].lastName + "</div>" +
             // "<div class='d-flex flex-column'>" +
             "<a href='/team/" + team + "/add/" + users[i].id + "' " + "class='btn btn-outline-info btn-sm align-self-end'>Add User</a>" +
             // "</div>" +
             // "</div>" +
-            "</div>" +
-            "<hr>";
+            "</div>";
     }
 }
 showUsers(users);
